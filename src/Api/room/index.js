e3a890b12fff20dfbbbd544a23c90eb98f0bf7a2
waouo <f5864565@gmail.com>
2021-01-28 14:51:08 +0800

    rooms's API

---
+++ src/Api/room/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const cors = 'https://cors-anywhere.herokuapp.com/'
+const URL = 'https://challenge.thef2e.com/api/thef2e2019/stage6'
+
+const roomRequest = axios.create({
+  baseURL: `${URL}`,
+  headers: {
+    Authorization: `Bearer ${process.env.MY_F2E_TOKEN}`,
+    Accept: 'application/json',
+  },
+})
+
+export const getRoomsApi = () => roomRequest.get('/rooms')
+export const getRoomDetailsApi = (id) => roomRequest.get(`/room/${id}`)
+export const reservedRoomApi = (id, data) => roomRequest.post(`/room/${id}`, data)
+export const deleteRoomsApi = () => roomRequest.delete(`/rooms`)

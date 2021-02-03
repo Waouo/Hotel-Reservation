ffa0ad8e93164b528f4fc719569fbda39f165b67
waouo <f5864565@gmail.com>
2021-02-03 15:24:39 +0800

    HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,21 +1,40 @@
 import { useEffect, useState } from 'react'
-import styles from './Home.module.scss'
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group'
+import styles from './Home.scss'
 import { SlideItemsContext } from '../../contexts'
-import bgImages from './bgImages'
+import bgImagesPath from './bgImagesPath'
 import Sidebar from '../../components/Sidebar'
 import Gallery from '../../components/Gallery'
 import Footer from '../../components/Footer'
 
 const HomePage = () => {
   const [bgNum, setBgNum] = useState(0)
-  const [homeStyle, setHomeStyle] = useState(bgImages[bgNum])
+  const [homeStyle, setHomeStyle] = useState({
+    backgroundImage: `url(${bgImagesPath[bgNum]})`,
+  })
+  let srcBg = bgImagesPath[bgNum]
 
   useEffect(() => {
-    setHomeStyle(bgImages[bgNum])
+    setHomeStyle({
+      backgroundImage: `url(${bgImagesPath[bgNum]})`,
+    })
   }, [bgNum])
 
   return (
-    <div className={styles.home} style={homeStyle}>
+    <div className={styles.home}>
+      <TransitionGroup component={null}>
+        <CSSTransition
+          classNames="animation-fade"
+          timeout={500}
+          key={bgNum}
+        >
+            <img className={styles.bg} src={srcBg} />
+        </CSSTransition>
+      </TransitionGroup>
+
       <div className={styles.home__container}>
         <Sidebar />
         <Gallery />
---
+++ src/pages/HomePage/bgImagesPath.js
@@ -0,0 +1,12 @@
+const bgImage_0 =
+  '../../../public/images/house/photo-1507149833265-60c372daea22.jpeg'
+const bgImage_1 =
+  '../../../public/images/house/photo-1549490148-d7304e934d25.jpeg'
+const bgImage_2 =
+  '../../../public/images/house/photo-1523217582562-09d0def993a6.jpeg'
+const bgImage_3 =
+  '../../../public/images/house/photo-1551516594-56cb78394645.jpeg'
+
+const bgImagesPath = [bgImage_0, bgImage_1, bgImage_2, bgImage_3]
+
+export default bgImagesPath

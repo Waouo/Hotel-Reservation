a0732618b0436a4b3dc9b1a3517a2e2a9aae86b8
waouo <f5864565@gmail.com>
2021-02-18 12:14:40 +0800

    SubmitButton

---
+++ src/components/SubmitButton/SubmitButton.jsx
@@ -0,0 +1,20 @@
+import styles from './SubmitButton.scss'
+import classNames from 'classnames/bind'
+import PropTypes from 'prop-types'
+
+const cx = classNames.bind(styles)
+
+const SubmitButton = ({ background, children }) => {
+  return (
+    <button className={cx('submit-button')} style={{ background }}>
+      {children}
+    </button>
+  )
+}
+
+SubmitButton.propTypes = {
+  background: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+}
+
+export default SubmitButton

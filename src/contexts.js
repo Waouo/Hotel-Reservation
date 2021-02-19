aa45f5f1a2e2717b3174a9730c879faa99b0f3c6
waouo <f5864565@gmail.com>
2021-02-19 17:05:57 +0800

    BookingContext

--- src/contexts.js
+++ src/contexts.js
@@ -2,3 +2,5 @@ import { createContext } from 'react'
 
 export const RoomsContext = createContext(null)
 export const SlideItemsContext = createContext(null)
+export const BookingContext = createContext(null)
+

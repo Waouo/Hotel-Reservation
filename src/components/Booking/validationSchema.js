274db54f4fa3fb88562fc84aab7f9cadda83c152
waouo <f5864565@gmail.com>
2021-02-23 17:00:10 +0800

    新增手機號碼長度驗證

--- src/components/Booking/validationSchema.js
+++ src/components/Booking/validationSchema.js
@@ -2,5 +2,12 @@ import * as Yup from 'yup'
 
 export default Yup.object({
   name: Yup.string().required('請輸入姓名'),
-  tel: Yup.number().required('請輸入手機號碼').typeError('請輸入正確的手機號碼格式'),
+  tel: Yup.number()
+    .test(
+      'len',
+      '請輸入輸入10碼手機號碼',
+      (val) => val.toString().length === 10
+    )
+    .typeError('請輸入正確的手機號碼格式')
+    .required('請輸入手機號碼'),
 })

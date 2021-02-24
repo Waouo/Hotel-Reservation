bd8df00533af1b64374234bb59f6da99e5c00e8e
waouo <f5864565@gmail.com>
2021-02-24 15:53:12 +0800

    修正送出資料後會產生val為undfined的BUG

--- src/components/Booking/validationSchema.js
+++ src/components/Booking/validationSchema.js
@@ -3,11 +3,10 @@ import * as Yup from 'yup'
 export default Yup.object({
   name: Yup.string().required('請輸入姓名'),
   tel: Yup.number()
-    .test(
-      'len',
-      '請輸入輸入10碼手機號碼',
-      (val) => val.toString().length === 10
-    )
+    .test('tel', '請輸入輸入10碼手機號碼', (val) => {
+      if (val) {
+        return val.toString().length === 10
+      }
+    })
     .typeError('請輸入正確的手機號碼格式')
-    .required('請輸入手機號碼'),
 })

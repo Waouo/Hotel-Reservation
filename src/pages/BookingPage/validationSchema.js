87027bdd9aed8e5f9db26bb70b85a1e2311fa278
Waouo <66577859+Waouo@users.noreply.github.com>
2021-03-16 22:49:17 +0800

    手機號碼驗證

--- src/pages/BookingPage/validationSchema.js
+++ src/pages/BookingPage/validationSchema.js
@@ -2,11 +2,13 @@ import * as Yup from 'yup'
 
 export default Yup.object({
   name: Yup.string().required('請輸入姓名'),
-  tel: Yup.number()
-    .test('tel', '請輸入輸入10碼手機號碼', (val) => {
-      if (val) {
-        return val.toString().length === 10
-      }
-    })
-    .typeError('請輸入正確的手機號碼格式')
+  tel: Yup.string().test('tel', '請輸入輸入10碼手機號碼', function (val) {
+    if (/\D/gi.test(val)) {
+      return this.createError({ message: '請輸入正確的手機格式' })
+    }
+
+    if (val) {
+      return val.toString().trim().length === 10
+    }
+  }),
 })

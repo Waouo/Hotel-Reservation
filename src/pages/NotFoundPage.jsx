952ad0d331e740d44d2067dfbc6325d79fe07955
waouo <f5864565@gmail.com>
2021-04-22 16:53:16 +0800

    增加_mixin.scss檔，增加圖片遮罩: after_conver

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,65 +2,28 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import 'react-date-range/dist/styles.css' // main style file
 import 'react-date-range/dist/theme/default.css' // theme css file
-import { DateRange } from 'react-date-range'
 import dayjs from 'dayjs'
 
-const fmt = 'YYYY-MM-DD'
-const tomorrow = dayjs().startOf('day').add(1, 'day')
-
 const NotFoundPage = () => {
-  const [state, setState] = useState([
-    {
-      startDate: tomorrow.toDate(),
-      endDate: tomorrow.add(1, 'day').toDate(),
-      key: 'selection',
-      isPickingStartDate: false,
-      isPickingEndDate: false,
-    },
-  ])
-
-  const [days, setDays] = useState('')
-
-  useEffect(() => {
-    const start = state[0].startDate
-    const end = state[0].endDate
-    setDays(dayjs(state[0].endDate).diff(dayjs(state[0].startDate), 'day'))
-    console.log('////////////////////////////////////')
-    console.log('start', dayjs(state[0].startDate))
-    console.log('end', dayjs(state[0].endDate))
-    console.log(dayjs(state[0].startDate).format(fmt))
-
-    function getDate() {
-      let start = dayjs(state[0].startDate)
-      let end = dayjs(state[0].endDate)
-      let newArr = [start.format(fmt)]
-
-      while (start.format(fmt) !== end.format(fmt)) {
-        start = start.add(1, 'day')
-        newArr.push(start.format(fmt))
+  var xorOperation = function (n, start) {
+    let result
+    
+    Array.from({ length: n }, (_, i) => start + 2 * i).forEach((val) => {
+      if (!result) {
+        result = val
+      } else {
+        result = result ^ val
       }
-      return newArr
-    }
-    console.log('getDate: ', getDate())
-  }, [state, days])
+    })
+
+    return result
+  }
+  console.log(xorOperation(4, 3))
 
   return (
-    <div>
+    <div style={{ background: 'black' }}>
       <h1 className="a">Page Not Found</h1>
       <Link to="/">Go back to home</Link>
-      <DateRange
-        showDateDisplay={false}
-        showMonthAndYearPickers={false}
-        editableDateInputs={false}
-        onChange={(item) => setState([item.selection])}
-        moveRangeOnFirstSelection={false}
-        ranges={state}
-        rangeColors={['#38470b']}
-        minDate={tomorrow.toDate()}
-        maxDate={tomorrow.add(88, 'day').toDate()}
-        months={2}
-        direction="horizontal"
-      />
     </div>
   )
 }

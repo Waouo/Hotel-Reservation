88716f1b7e745560d5f5f7a7080aad78a123b638
waouo <f5864565@gmail.com>
2021-06-30 11:08:29 +0800

    修改BookingPage動畫

--- src/pages/RoomPage/RoomPage.jsx
+++ src/pages/RoomPage/RoomPage.jsx
@@ -27,14 +27,8 @@ const RoomPage = ({ match }) => {
   const bgObj = useBackGround(room.imageUrl)
 
   //BookingContext
-  const {
-    dateArr,
-    state,
-    nightsObj,
-    bookingArr,
-    setState,
-    tomorrow,
-  } = useCalendarStatus(booking)
+  const { dateArr, state, nightsObj, bookingArr, setState, tomorrow } =
+    useCalendarStatus(booking)
   const [totalPrice, setTotalPrice] = useState(0)
 
   useEffect(() => {
@@ -58,106 +52,101 @@ const RoomPage = ({ match }) => {
     >
       <RoomsContext.Provider value={{ room, des, setShowBooking }}>
         <div className={cx('room-details')}>
-          <TransitionGroup component={null}>
-            <CSSTransition
-              classNames="animation-fade"
-              timeout={{
-                appear: 1000,
-                enter: 1000,
-                exit: 0,
-              }}
-              key={showBooking}
-            >
-              <div className={cx('booking-page', { show: showBooking })}>
-                <BookingPage />
+          <CSSTransition
+            in={showBooking}
+            classNames="animation-fade"
+            timeout={1000}
+            unmountOnExit
+          >
+            <div className={cx('booking-page')}>
+              <BookingPage />
             </div>
           </CSSTransition>
-        </TransitionGroup>
-        <div className={cx('room-container', 'row')}>
-          <section className={cx('panel', 'col-md-5', 'col-12')}>
-            <TransitionGroup component={null}>
-              <CSSTransition
-                classNames="animation-fade"
-                timeout={1000}
-                key={bgObj?.num}
-              >
-                <img
-                  className={cx('bg')}
-                  src={bgObj?.src}
-                  alt={'background-image'}
+          <div className={cx('room-container', 'row')}>
+            <section className={cx('panel', 'col-md-5', 'col-12')}>
+              <TransitionGroup component={null}>
+                <CSSTransition
+                  classNames="animation-fade"
+                  timeout={1000}
+                  key={bgObj?.num}
+                >
+                  <img
+                    className={cx('bg')}
+                    src={bgObj?.src}
+                    alt={'background-image'}
+                  />
+                </CSSTransition>
+              </TransitionGroup>
+              <Link to="/" className={cx('prePage')}>
+                <span className={cx('arrow')}> &lt; </span>查看其它房型
+              </Link>
+              <div className={cx('booking')}>
+                <div style={{ width: '252px' }}>
+                  <h2 className={cx('booking-price')}>
+                    <span>${totalPrice || null}</span>
+                    &nbsp; / &nbsp; {nightsObj.normal + nightsObj.holiday}晚
+                  </h2>
+                  <a href="#booking" style={{ margin: '0 auto' }}>
+                    <SubmitButton onClick={() => setShowBooking(true)}>
+                      Booking now
+                    </SubmitButton>
+                  </a>
+                </div>
+                <Carousel
+                  num={3}
+                  variable={bgObj?.num}
+                  setVariable={bgObj?.setNum}
+                  color={'light-green'}
                 />
-              </CSSTransition>
-            </TransitionGroup>
-            <Link to="/" className={cx('prePage')}>
-              <span className={cx('arrow')}> &lt; </span>查看其它房型
-            </Link>
-            <div className={cx('booking')}>
-              <div style={{ width: '252px' }}>
-                <h2 className={cx('booking-price')}>
-                  <span>${totalPrice || null}</span>
-                  &nbsp; / &nbsp; {nightsObj.normal + nightsObj.holiday}晚
-                </h2>
-                <a href="#booking" style={{ margin: '0 auto' }}>
-                  <SubmitButton onClick={() => setShowBooking(true)}>
-                    Booking now
-                  </SubmitButton>
-                </a>
               </div>
-              <Carousel
-                num={3}
-                variable={bgObj?.num}
-                setVariable={bgObj?.setNum}
-                color={'light-green'}
-              />
-            </div>
-          </section>
-          <main className={cx('info', ' col-md-7', 'col-12')}>
-            <div className={cx('info-container')}>
-              <h1 className={cx('room-name')}>
-                {room.name}
-                <RoomSize />
-              </h1>
-              <ul className={cx('time')}>
-                <li>
-                  平日（一～四）價格：${room.normalDayPrice} /
-                  假日（五〜日）價格： ${room.holidayPrice}
-                </li>
-                <li>
-                  {' '}
+            </section>
+            <main className={cx('info', ' col-md-7', 'col-12')}>
+              <div className={cx('info-container')}>
+                <h1 className={cx('room-name')}>
+                  {room.name}
                   <RoomSize />
-                </li>
-                <li>
-                  入住時間：
-                  {room.checkInAndOut?.checkInEarly}
-                  （最早）/ {room.checkInAndOut?.checkInLate}（最晚）
-                </li>
-                <li>退房時間：{room.checkInAndOut?.checkOut}</li>
-              </ul>
-              <ul className={cx('description')}>
-                <li>
-                  {room.name}{' '}
-                  {des.GuestMax === 1
-                    ? '僅供一位客人'
-                    : `供${des.GuestMin || ''}~${des.GuestMax || ''}人使用`}
-                  使用。
-                </li>
-                <li>
-                  臥室配有
-                  {des.Bed && des.Bed.length > 1 && `${des?.Bed.length}張 `}
-                  {des.Bed && `${des.Bed[0]} Bed `}和私人浴室。
-                </li>
-                <li>
-                  您需要的一切為您準備：床單和毯子，毛巾，肥皂和洗髮水，吹風機。
-                </li>
-                <li>房間裡有 AC，當然還有 WiFi。</li>
-              </ul>
-              <Amenities room={room} />
-              <h2 className={cx('room-empty-status')}>空房間狀態查詢</h2>
-              <Calendar />
-            </div>
-          </main>
+                </h1>
+                <ul className={cx('time')}>
+                  <li>
+                    平日（一～四）價格：${room.normalDayPrice} /
+                    假日（五〜日）價格： ${room.holidayPrice}
+                  </li>
+                  <li>
+                    {' '}
+                    <RoomSize />
+                  </li>
+                  <li>
+                    入住時間：
+                    {room.checkInAndOut?.checkInEarly}
+                    （最早）/ {room.checkInAndOut?.checkInLate}（最晚）
+                  </li>
+                  <li>退房時間：{room.checkInAndOut?.checkOut}</li>
+                </ul>
+                <ul className={cx('description')}>
+                  <li>
+                    {room.name}{' '}
+                    {des.GuestMax === 1
+                      ? '僅供一位客人'
+                      : `供${des.GuestMin || ''}~${des.GuestMax || ''}人使用`}
+                    使用。
+                  </li>
+                  <li>
+                    臥室配有
+                    {des.Bed && des.Bed.length > 1 && `${des?.Bed.length}張 `}
+                    {des.Bed && `${des.Bed[0]} Bed `}和私人浴室。
+                  </li>
+                  <li>
+                    您需要的一切為您準備：床單和毯子，毛巾，肥皂和洗髮水，吹風機。
+                  </li>
+                  <li>房間裡有 AC，當然還有 WiFi。</li>
+                </ul>
+                <Amenities room={room} />
+                <h2 className={cx('room-empty-status')}>空房間狀態查詢</h2>
+                <Calendar />
+              </div>
+            </main>
+          </div>
         </div>
-      </div>
       </RoomsContext.Provider>
     </BookingContext.Provider>
   )

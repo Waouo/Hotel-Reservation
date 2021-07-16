ceaf7d4af0a726b902e90f0a2d91bd90da016fe6
Waouo <66577859+Waouo@users.noreply.github.com>
2021-07-16 15:27:44 +0800

    準備發布到heroku

--- server/server.js
+++ server/server.js
@@ -10,6 +10,6 @@ const __dirname = path.resolve()
 
 app.use(express.static(path.join(__dirname, '..', 'dist')))
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server running on ${port} port!`))

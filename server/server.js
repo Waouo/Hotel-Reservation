ee2402d7b8935ea11b28a1501b9e262fc77b842f
Waouo <66577859+Waouo@users.noreply.github.com>
2021-07-16 14:44:27 +0800

    準備發布到heroku

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 
 dotenv.config()
 
 const app = express()
 
-app.use(express.static('../dist'))
+const __dirname = path.resolve()
+
+app.use(express.static(path.join(__dirname, '..', 'dist')))
 
 const port = 5000
 
